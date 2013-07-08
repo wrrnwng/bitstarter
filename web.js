ca62ec116a951c0cbcfc9abf2a77953ce5c59caa
Ubuntu <ubuntu@ip-10-164-87-140.ec2.internal>
2013-07-08 14:29:28 +0000

    update web.js to read in index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buff = new Buffer(fs.readFileSync(index.html));
+var buff = new Buffer(fs.readFileSync('index.html'));
 
 app.get('/', function(request, response) {
   response.send(buff.toString());
